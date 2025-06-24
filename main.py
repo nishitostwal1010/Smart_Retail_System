d7bcb2a38b36eb2927afa9c71a9cab9a52fe740d
nishitostwal1010 <157974203+nishitostwal1010@users.noreply.github.com>
2025-06-24 21:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov12n.pt')  # load a pretrained model (recommended for training)
 
 model.train(data='C:/Users/nishi/PycharmProjects/pythonProject/data/dataset', epochs=20, imgsz=64)
